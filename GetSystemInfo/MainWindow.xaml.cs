070e07279832481f00088f3205b01c3b2609273c
Adam Braden <adambr@microsoft.com>
2021-11-08 09:19:00 -0800

    WinAppSDK updates

--- GetSystemInfo/MainWindow.xaml.cs
+++ GetSystemInfo/MainWindow.xaml.cs
@@ -2,26 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Windows.Graphics.Display;
+//Windows WinRT APIs
 using Windows.System.Profile;
 using Windows.Security.Cryptography;
 using Windows.Devices.Display;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
-using System.Diagnostics;
-using System.Windows.Interop;
-using Windows.Security.Credentials.UI;
+using WinRT.Interop;
+
 
 namespace GetSystemInfo
 {
@@ -36,9 +27,38 @@ namespace GetSystemInfo
         {
             InitializeComponent();
         }
+        private async void SystemInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var sb = new StringBuilder();
+
+            //Get System Identifier
+            var systemId = SystemIdentification.GetSystemIdForPublisher();
+
+            //Get info about current DisplayMonitor
+            var devinfoDisplays = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(DisplayMonitor.GetDeviceSelector());
+
+            // Show system id and specific display monitor values
+            sb.Append($"\nSystemId: ({systemId.Source})\n {CryptographicBuffer.EncodeToHexString(systemId.Id).ToUpper().Substring(0, 10)}");
+            sb.Append("\n\nDisplay Info:\n");
+            foreach (var devInfo in devinfoDisplays)
+            {
+                var displayMonitor = await DisplayMonitor.FromInterfaceIdAsync(devInfo.Id);
+                sb.Append($" Name:{displayMonitor.DisplayName}\n DeviceId: {displayMonitor.DeviceId}\n Connector: {displayMonitor.PhysicalConnector}\n Size: {displayMonitor.PhysicalSizeInInches}");
+            }
+
+            //// Windows App SDK APIs
+            //var displayPower = Microsoft.Windows.System.Power.PowerManager.DisplayStatus;
+            //var energySaver = Microsoft.Windows.System.Power.PowerManager.EnergySaverStatus;
+            //sb.Append($"\n\n Display Status: {displayPower}");
+            //sb.Append($"\n Energy Saver: {energySaver}");
+
+            SystemInfo.Text = sb.ToString();
+
+        }
 
         private async void TakePicture_Click(object sender, RoutedEventArgs e)
         {
+
             //Initialize Camera
             captureManager = new MediaCapture();
             await captureManager.InitializeAsync();
@@ -53,43 +73,28 @@ namespace GetSystemInfo
             await captureManager.CapturePhotoToStorageFileAsync(imgFormat, file);
 
             // Get photo as a BitmapImage
-            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+            _ = new BitmapImage(new Uri(file.Path));
 
             // imagePreview is a <Image> object defined in XAML
-            imagePreview.Source = bmpImage;
+            //imagePreview.Source = bmpImage;
         }
 
-        private async void SystemInfo_Click(object sender, RoutedEventArgs e)
-        {
-            var sb = new StringBuilder();
-
-            //Get System Identifier
-            var systemId = SystemIdentification.GetSystemIdForPublisher();
-            
-            //Get OS Flighting info
-            var attrNames = new List<string>{ "DeviceFamily", "OSVersionFull", "FlightRing" };
-            var attrData = await AnalyticsInfo.GetSystemPropertiesAsync(attrNames);
-
-            //Format output
-            sb.Append($"\nSystemId: ({systemId.Source})\n {CryptographicBuffer.EncodeToHexString(systemId.Id).ToUpper().Substring(0,10)}\n");
-            sb.Append("\nWindows Build Info:");
-            foreach (KeyValuePair<string, string> attr in attrData)
-            {
-                sb.Append($"\n{attr.Key}={attr.Value}");
-            }
-
-            //Get info about current DisplayMonitor
-            var devinfoDisplays = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(DisplayMonitor.GetDeviceSelector());
-
-            //Query specific values and display them
-            sb.Append("\n\nDisplay Info:\n");
-            foreach (var devInfo in devinfoDisplays)
-            {
-                var displayMonitor = await DisplayMonitor.FromInterfaceIdAsync(devInfo.Id);
-                sb.Append($" Name:{displayMonitor.DisplayName}\n DeviceId: {displayMonitor.DeviceId}\n Connector: {displayMonitor.PhysicalConnector}\n Size: {displayMonitor.PhysicalSizeInInches}");
-            }
-
-            SystemInfo.Text = sb.ToString();
-        }
     }
 }
+
+
+
+
+
+
+////Get Windows Insider Preview OS Flighting info
+//var attrNames = new List<string>{ "DeviceFamily", "OSVersionFull", "FlightRing" };
+//var attrData = await AnalyticsInfo.GetSystemPropertiesAsync(attrNames);
+
+////Format output
+//sb.Append($"\nSystemId: ({systemId.Source})\n {CryptographicBuffer.EncodeToHexString(systemId.Id).ToUpper().Substring(0,10)}\n");
+//sb.Append("\nWindows Build Info:");
+//foreach (KeyValuePair<string, string> attr in attrData)
+//{
+//    sb.Append($"\n{attr.Key}={attr.Value}");
+//}
